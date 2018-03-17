bd9a79daaf7a424a2b0907db02b36c78c9c67348
nogang <nogangbaby@gmail.com>
2018-03-17 13:21:21 +0900

    make fund

--- Web/routes/raiseFundTest.js
+++ Web/routes/raiseFundTest.js
@@ -7,45 +7,39 @@ var web3 = new Web3();
 //var Pudding = require('ether-pudding');
 web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/gGHwulfhVK8ouWn8aZMz'));	//'http://192.168.1.5:8545'
 
-//var code = fs.readFileSync('simplestorage.sol').toString();
-//var contract =  web3.eth.compile.solidity(code);
-function teste (){
-	alert("ccc");
-
-}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('raiseFundTest', { title: 'raiseFundTest'
+  res.render('raiseFundTest', { title: 'raiseFundTest',
+										lodingPage : '<div id="load" style="display:none; position:absolute; top:0px; left:0px; z-index:1;">',
+										inputPage : '<div id="fundingInfo" style="display:block; position:absolute; top:0px; left:0px; z-index:1;">',
+										byteCode: '0',
+										abiDefinition: '0',
+										tryPublishFund: 0
 						});
 });
+//visibility:hidden <-> visible
 
 
 router.post('/', function(req, res) {
-	console.log("a");
+
 	var input = fs.readFileSync('../../LoFun/Solidity/FundBasic.sol',{encoding: 'utf8'}).toString();//.replace(/\n/g,' ');
-	console.log("b");
-	//var compiled = solc.compile(source, 1);
-	console.log(input);
-	//console.log(compiled);
-	console.log("d");
-	//console.log(compiled);
-
-	//var input = 'contract x { function g() {} }';
-	// Setting 1 as second paramateractivates the optimiser
 	var output = solc.compile(input, 1);
+
 	for (var contractName in output.contracts) {
-	    // code and ABI that are needed by web3
-	    console.log(contractName + ': ' + output.contracts[contractName].bytecode);
-	    console.log(contractName + '; ' + JSON.parse(output.contracts[contractName].interface));
+			var abiDefinition = JSON.parse(output.contracts[contractName].interface);
+			console.log(contractName + ': ' + abiDefinition);
+
+			var byteCode = output.contracts[contractName].bytecode;
+			console.log(contractName + '; ' + byteCode);
+
+			res.render('raiseFundTest', { title: 'raiseFundTest',
+												lodingPage : '<div id="load" style="display:block">',
+												inputPage : '<div id="fundingInfo" style="display:none">',
+												byteCode: byteCode,
+												abiDefinition: abiDefinition,
+												tryPublishFund: 1
+		  });
 	}
-
-	//if(!compiled.contracts["FundBasic"]) {
-//    console.log('Contract must have same name as file!');
-//}
-	//console.log(req.body.fundingname);
-	//res.send("ddd");
-
-
 });
 
 module.exports = router;
